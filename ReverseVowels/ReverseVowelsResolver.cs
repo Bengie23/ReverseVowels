1d3b1b04f6c14d1d2e56a82457f03e15242e6ab6
Bengie23 <benibenancio17@gmail.com>
2021-02-14 13:43:22 -0600

    Add project files.

---
+++ ReverseVowels/ReverseVowelsResolver.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseVowels
+{
+    public class ReverseVowelsResolver
+    {
+        private Stack<char> vowelsFound = new Stack<char>();
+        private string stringToEvaluate;
+        private char[] vowels = { 'a','e','i','o','u' };
+
+        internal ReverseVowelsResolver(string stringParameter)
+        {
+            if (stringParameter is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.stringToEvaluate = stringParameter;
+        }
+
+        internal string TryResolve()
+        {
+            char[] arrayOfChar = stringToEvaluate.ToCharArray();
+
+            if (!arrayOfChar.Where(x => EvaluateIsVowelAndStore(x)).ToArray().Any()) {
+                return new string(arrayOfChar);
+            }
+
+
+            var result = arrayOfChar.Select(x =>
+             {
+                 if (IsVowel(x))
+                 {
+                     return vowelsFound.Pop();
+                 }
+                 else
+                 {
+                     return x;
+                 }
+             }).ToArray();
+
+            return new string(result);
+        }
+
+        internal bool EvaluateIsVowelAndStore(char c)
+        {
+            bool isVowel = IsVowel(c);
+            if (isVowel)
+            {
+                vowelsFound.Push(c);
+            }
+            return isVowel;
+        }
+
+        internal bool IsVowel(char c)
+        {
+            char value = c.ToString().ToLower()[0];
+            return vowels.Contains(value);
+
+            
+        }
+
+        public static string Resolve(string stringParameter)
+        {
+            var resolvable = new ReverseVowelsResolver(stringParameter);
+
+            return resolvable.TryResolve();
+
+        }
+    }
+}
---
+++ ReverseVowelsTests/ReverseVowelsTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReverseVowels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseVowelsTests
+{
+    [TestClass]
+    public class ReverseVowelsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReverseVowels_Null_String_Throws()
+        {
+            ReverseVowelsResolver.Resolve(null);
+        }
+
+        [TestMethod]
+        public void ReverseVowels_Empty_String_Returns()
+        {
+            string result = ReverseVowelsResolver.Resolve("");
+
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        [DataRow("hello","holle")]
+        [DataRow("hello, world","hollo, werld")]
+        [DataRow("codesignal", "cadisegnol")]
+        [DataRow("eIaOyU", "UOaIye")]
+        [DataRow(".a", ".a")]
+        [DataRow("ai", "ia")]
+        [DataRow("aA", "Aa")]
+        [DataRow(".,", ".,")]
+        [DataRow("ab", "ab")]
+        [DataRow("0P", "0P")]
+        [DataRow("a a", "a a")]
+        [DataRow("a e", "e a")]
+        [DataRow("race a car", "raca e car")]
+        [DataRow("Sore was I ere I saw Eros.", "SorE was I ere I saw eros.")]
+        [DataRow("A man, a plan, a canal -- Panama", "a man, a plan, a canal -- PanamA")]
+        public void ReverseVowels_Returns(string value,string expectedResult)
+        {
+            string result = ReverseVowelsResolver.Resolve(value);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
